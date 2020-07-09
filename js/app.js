cd88bdc946224fc11ba858ce769a106d36e8dbc0
Julian Toledo <45986631+JulianToledoGT@users.noreply.github.com>
2020-07-08 22:43:41 -0600

    Movimiento de elementos
    Ya se pueden intercambiar los elemntos, falta limitar para que solo permita intercambiar con vecinos horizontales y verticales, implementar contadores de movimientos , verificar cuando hayan 4 en elementos iguales continuos para eliminarlos y rellenar los espacios y finalmente mostrar estadísticas del juego.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 var miColor = $('.main-titulo').css('color');
 var min = 2;
 var seg = 0;
-var jugando = 0;
+var ori = "";
+var dst = "";
 
-setInterval(function(){tiempo()}, 50);
+if (sessionStorage.getItem("iniciado")){}else{sessionStorage.setItem("iniciado", 0)};
+
+setInterval(function(){tiempo()}, 100);
 
 function tiempo() {
-    if(miColor == 'rgb(220, 255, 14)')
-        {miColor = 'rgb(255, 255, 255)'}
-    else
-        {miColor = 'rgb(220, 255, 14)'};
+    if(miColor == 'rgb(220, 255, 14)') {
+        miColor = 'rgb(255, 255, 255)'
+    } else {
+        miColor = 'rgb(220, 255, 14)'
+    };
     $('.main-titulo').css('color', miColor);
-    if (jugando==1){
+    if (sessionStorage.getItem("iniciado")==1){
         if (seg <= 0) {
             if (min <= 0) {
                     min = 2;
                     seg = 0;
-                    jugando = 0;
+                    sessionStorage.setItem("iniciado", 0);
             } else {
                     min -= 1;
                     seg = 59;
@@ -33,32 +37,51 @@ function tiempo() {
 };
 
 function enteroAleatorio(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
 function iniciarJuego() {
-    jugando = 1;
-    var filas = 6;
-	var columnas = $('[class^="col-"]');
+  if (sessionStorage.getItem("iniciado")==1){
+    
+    var columnas = $('[class^="col-"]');
 
-    columnas.each(
-        function () {
-            var dulces = $(this).children().length;
-            var agrega = filas - dulces;
-            for (var i = 0; i < agrega; i++) {
-                var dulceTipo = enteroAleatorio(1, 5);
-                $(this).append('<img src="image/' + dulceTipo + '.png" class="element"></img>');
-            };
-        }
-    );
+    columnas.each(function() {
+      for (var i = 0; i < 6; i++) {
+        var color = enteroAleatorio(1, 5);
+        $(this).append('<div id="a' + $(this).attr("class") + i + '" class="elemento"><div id="b' + $(this).attr("class") + i + '" class="draggable dulce' + color + '"></div></div>');
+      };
+    });
+  }
 };
 
 $(function() {
-	$('.btn-reinicio').click(function () {
-        if (jugando == 0){
-            iniciarJuego();
-        } else {
-            jugando = 0;
-        };
-	});
+  iniciarJuego();
+
+	$('.btn-reinicio').click(function(){
+    sessionStorage.setItem("iniciado", 1);
+    location.reload();
+  });
+
+  $(".draggable").draggable({
+    revert: true,
+    drag: function(event, ui){
+      $(ui.draggable)
+      ori = $(this).parent().attr("id")
+    }
+  });
+
+  $(".elemento").droppable({
+    accept: ".draggable",
+    drop: function(event, ui) {
+      dst = $(this).attr("id")
+      if(dst != ori) {
+        $("#" + $("#" + dst + " > div").attr("id")).appendTo($("#" + ori));
+        $(ui.draggable)
+          .css({left: "auto", top: "auto"})
+          .appendTo($(this))
+      };
+    }
+  });
 });
+
+// Julian Toledo - 20200708 10:30 +/-
