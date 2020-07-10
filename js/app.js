3410175e7d2214b22506c2711c0f0a0691a960e4
Julian Toledo <45986631+JulianToledoGT@users.noreply.github.com>
2020-07-10 00:16:47 -0600

    Botón reinicio y efecto de fin de juego
    Falta terminar la animación de fin de juego, luego la comprobación de los elementos a eliminar, el llenado de las posiciones que queden vacantes y la contabilización de puntos. Además analizar si se permitirán combinaciones en llenado inicial o se evitarán.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var miColor = $('.main-titulo').css('color'); //Color Inicial de la etiqueta "Match Game" que permanecerá alternando durante la presentación de la página.
+var clr = $('.main-titulo').css('color'); //Color inicial de la etiqueta "Match Game" que permanecerá alternando durante la presentación de la página.
 var min = 2;  //Minutos (valor inicial y máximo pues al inicial el juego se reinicia a cero)
 var seg = 0;  //Segundos (valor inicial)
 var ori = ""; //Almacena la referencia a la posición de origen de un elemento
@@ -10,31 +10,32 @@ if (sessionStorage.getItem("jugando")){}else{sessionStorage.setItem("jugando", 0
 setInterval(function(){tiempo()}, 100);
 
 function tiempo() {
-    if(miColor == 'rgb(220, 255, 14)') {
-        miColor = 'rgb(255, 255, 255)'
+  if(clr == 'rgb(220, 255, 14)') {
+    clr = 'rgb(255, 255, 255)'
+  } else {
+    clr = 'rgb(220, 255, 14)'
+  };
+  $('.main-titulo').css('color', clr);
+  if (sessionStorage.getItem("jugando")==1){
+    if (seg <= 0) {
+      if (min <= 0) {
+        min = 2;
+        seg = 0;
+        sessionStorage.setItem("jugando", 0);
+        finalizaJuego();
+      } else {
+        min -= 1;
+        seg = 59;
+      };
     } else {
-        miColor = 'rgb(220, 255, 14)'
+      seg -= 1
     };
-    $('.main-titulo').css('color', miColor);
-    if (sessionStorage.getItem("jugando")==1){
-        if (seg <= 0) {
-            if (min <= 0) {
-                    min = 2;
-                    seg = 0;
-                    sessionStorage.setItem("jugando", 0);
-            } else {
-                    min -= 1;
-                    seg = 59;
-            };
-        } else {
-            seg -= 1
-        };
-        if (seg > 9) {
-            document.getElementById("timer").textContent = "0" + min + ":" + seg;
-        } else {
-            document.getElementById("timer").textContent = "0" + min + ":0" + seg;
-        };
+    if (seg > 9) {
+      document.getElementById("timer").textContent = "0" + min + ":" + seg;
+    } else {
+      document.getElementById("timer").textContent = "0" + min + ":0" + seg;
     };
+  };
 };
 
 function enteroAleatorio(min, max) {
@@ -43,22 +44,28 @@ function enteroAleatorio(min, max) {
 
 function iniciarJuego() {
   if (sessionStorage.getItem("jugando")==1){
-    
     var columnas = $('[class^="col-"]');
-
     columnas.each(function() {
-      for (var i = 0; i < 6; i++) {
+      for (var i = 0; i < 7; i++) {
         var color = enteroAleatorio(1, 5);
         $(this).append('<div id="a' + $(this).attr("class") + i + '" class="elemento"><div id="b' + $(this).attr("class") + i + '" class="draggable dulce' + color + '"></div></div>');
       };
     });
+    $('#btn-reinicio').text('Reiniciar');
   }
+};
+
+function finalizaJuego() {
+	$("div.panel-tablero, div.time").effect("fold");
+	$("h1.main-titulo").addClass('title-over')
+		.text('Juego Terminado');
+	$("div.score, div.moves, div.panel-score").width('100%');
 };
 
 $(function() {
   iniciarJuego();
 
-	$('.btn-reinicio').click(function(){
+  $('#btn-reinicio').click(function(){
     sessionStorage.setItem("jugando", 1);
     location.reload();
   });
@@ -74,21 +81,24 @@ $(function() {
   $(".elemento").droppable({
     accept: ".draggable",
     drop: function(event, ui) {
-      dst = $(this).attr("id")
+      dst = $(this).attr("id");
       //Determino el desplazamiento para el elemento sabiendo que he identificado cada contenedor
       // con un número correlativo tanto de columna como de fila, por lo que mediante la obtención
       // de la diferencia entre la posición de origen y la de destino, puedo determinar
       // cuántos espacios se desplazaría el elemento y permito que se realice solo cuando este valor sea igual a uno.
+      //Lo anterior también quiere decir que no permitimos movimientos en diagonal.
       dsp = Math.abs(parseInt(ori.substring(5, 6))-parseInt(dst.substring(5, 6)))
-          + Math.abs(parseInt(ori.substring(6, 7))-parseInt(dst.substring(6, 7)))
+          + Math.abs(parseInt(ori.substring(6, 7))-parseInt(dst.substring(6, 7)));
       if(dst != ori && dsp == 1) {
+        var pnt = Number($('#movimientos-text').text()) + 1;
+        $('#movimientos-text').text(pnt);
         $("#" + $("#" + dst + " > div").attr("id")).appendTo($("#" + ori));
         $(ui.draggable)
           .css({left: "auto", top: "auto"})
           .appendTo($(this))
-      };
+        };
     }
   });
 });
 
-// Julian Toledo - 20200708 11:20 +/-
+// Julian Toledo - 20200710 00:10 +/-
