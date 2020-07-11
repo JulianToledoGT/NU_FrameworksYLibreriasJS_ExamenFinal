86768510b59d24b0c8140381ffd92bc282128cc5
Julian Toledo <45986631+JulianToledoGT@users.noreply.github.com>
2020-07-11 00:47:41 -0600

    Bases para control de matchs
    Se agrega una matriz para el control de cambios. Se divide el código en funciones reutilizables. Se hacen avances en algoritmos para el manejo de match, control de punteo y llenado de columnas, entre otras cosas.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 var clr = $('.main-titulo').css('color'); //Color inicial de la etiqueta "Match Game" que permanecerá alternando durante la presentación de la página.
 var min = 2;  //Minutos (valor inicial y máximo pues al inicial el juego se reinicia a cero)
 var seg = 0;  //Segundos (valor inicial)
+var dsp = 0;  //Despalzamiento solicitado para un elemento entre su posición de origen y la de destino
 var ori = ""; //Almacena la referencia a la posición de origen de un elemento
 var dst = ""; //Almacena la referencia a la posición de destino de un elemento
-var dsp = 0;  //Despalzamiento solicitado para un elemento entre su posición de origen y la de destino
+var mtz = []; //Matriz con el contenido del tablero
+var col = []; //Lista dulces en una columna
 
 if (sessionStorage.getItem("jugando")){}else{sessionStorage.setItem("jugando", 0)};
 
@@ -42,18 +44,44 @@ function enteroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
+function llenarColumna(obj, ini) {
+//TODO: Agregar animación para que parezca que los duclces están descendiendo  
+  let num = 7
+  if (ini == true){col = [];}
+  for (var i = 0; i < num; i++) {
+    let dulce = enteroAleatorio(1, 5);
+    $(obj).append('<div id="a' + $(obj).attr("class") + i + '" class="elemento"><div id="b' + $(obj).attr("class") + i + '" class="draggable dulce' + dulce + '"></div></div>');
+    col.push(dulce)
+  };
+};
+
+function llenarMatriz(ini) {
+  if (ini == true){mtz = [];}
+  var columnas = $('[class^="col-"]');
+  columnas.each(function() {
+    llenarColumna(this, ini);
+    mtz.push(col);
+  });
+};
+
+function verificarMatriz(){
+//TODO: recorrer la matriz y verificar si hay elementos qué eliminar (que generarán puntajes)
+//TODO: ir marcando los objetos del tablero y marcar las posiciones relacionadas a elementos eliminados (agregar una clase)
+//TODO: eliminar los objetos marcados con la clase (debe usarse una animación)
+//TODO: recorrer nuevamente la matriz y eliminar las posiciones marcadas
+//TODO: recorrer el listado de columnas y para cada una agregar el número de objetos restantes (debe aplicarse una animación para el desplazamiento de los objetos que desciendan)
+};
+
 function iniciarJuego() {
   if (sessionStorage.getItem("jugando")==1){
-    var columnas = $('[class^="col-"]');
-    columnas.each(function() {
-      for (var i = 0; i < 7; i++) {
-        var color = enteroAleatorio(1, 5);
-        $(this).append('<div id="a' + $(this).attr("class") + i + '" class="elemento"><div id="b' + $(this).attr("class") + i + '" class="draggable dulce' + color + '"></div></div>');
-      };
-    });
+    llenarMatriz(true);
     $('#btn-reinicio').text('Reiniciar');
   }
 };
+
+//para obtener todos los dulces de una columna (no una fila)
+//mtz.map(function(value,index) { return value[2]; })
+//$(this).append('<div id="a' + $(this).attr("class") + i + '" class="elemento"><div id="b' + $(this).attr("class") + i + '" class="draggable dulce' + dulce + '"></div></div>');
 
 function finalizaJuego() {
 	$("div.panel-tablero, div.time").effect("fold");
@@ -92,6 +120,7 @@ $(function() {
       if(dst != ori && dsp == 1) {
         var pnt = Number($('#movimientos-text').text()) + 1;
         $('#movimientos-text').text(pnt);
+//TODO: También necesito intercabiar el valor del contenido de la matriz        
         $("#" + $("#" + dst + " > div").attr("id")).appendTo($("#" + ori));
         $(ui.draggable)
           .css({left: "auto", top: "auto"})
@@ -101,4 +130,4 @@ $(function() {
   });
 });
 
-// Julian Toledo - 20200710 00:10 +/-
+// Julian Toledo - 20200711 00:45 +/-
